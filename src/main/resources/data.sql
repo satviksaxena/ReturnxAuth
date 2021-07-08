34e002f007cc522382e3e829aea9ab6f1b069f22
satviksaxena <satviksaxena1@gmail.com>
2021-07-08 20:37:15 +0530

    some edit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,2 @@
-insert into authentication_request (user_name, password) values ('adyasha1', 'adyasha1');
-insert into authentication_request (user_name, password) values ('abhishek2', 'abhishek2');
-insert into authentication_request (user_name, password) values ('satvik3', 'satvik3');
-insert into authentication_request (user_name, password) values ('amit4', 'amit4');
-insert into authentication_request (user_name, password) values ('someUser5', 'someUser5');
+insert into authentication_request (user_name, password) values ('visitor', 'visitor');
+insert into authentication_request (user_name, password) values ('satvik1', 'satvik1');
